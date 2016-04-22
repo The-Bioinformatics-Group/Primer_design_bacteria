420f535c9d6b2a62687239d16c6c029b2b2ebdfe
mtop <mats@topel.se>
2016-04-22 09:48:37 +0200

    Now with different output files

--- 4_blast/runBLAST.sh
+++ 4_blast/runBLAST.sh
@@ -4,7 +4,7 @@ REFDIR=/nobackup/data5/data/references/In-house_bacteria/skeletonema_bac_genomes
 
 blastn -task blastn-short -query primers.fst -db $REFDIR/Kordia_sp._nov/combined-revcomp-unitig-1-2.fasta -out primers_to_combined-revcomp-unitig-1-2.BLASTn-short.txt
 
-blastn -task blastn-short -query primers.fst -db $REFDIR/unitig_0/circularized_unitig_0_oneline.fasta -out primers_to_combined-revcomp-unitig-1-2.BLASTn-short.txt
+blastn -task blastn-short -query primers.fst -db $REFDIR/unitig_0/circularized_unitig_0_oneline.fasta -out primers_to_corized_unitig_0_oneline.BLASTn-short.txt
 
-blastn -task blastn-short -query primers.fst -db $REFDIR/genome_4/reassembled_genome_4_full.fasta -out primers_to_combined-revcomp-unitig-1-2.BLASTn-short.txt
+blastn -task blastn-short -query primers.fst -db $REFDIR/genome_4/reassembled_genome_4_full.fasta -out primers_to_reassembled_genome_4_full.BLASTn-short.txt
 
